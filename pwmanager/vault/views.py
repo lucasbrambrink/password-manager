0a5fcbcc5b79d30351b2520cb1d554904cc5daa7
lucasbrambrink <lbrambrink@gmail.com>
2017-02-18 21:35:58 -0500

    adds authentication view barebones

--- pwmanager/vault/views.py
+++ pwmanager/vault/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
+class AuthenticationView(TemplateView):
+    template_name = u'vault/authentication.html'
+
+    def get(self, request, *args, **kwargs):
+        """
+        only allow 3 attempts
+        perhaps perform some kind of MFA
+        """
+        # authenticate user
+        # perhaps return json response?
+        pass
+
+
 class VaultView(TemplateView):
     template_name = u'vault/vault.html'
 
@@ -9,3 +22,5 @@ class VaultView(TemplateView):
         return render(request, self.template_name, {
             u'passwords': []
         })
+
+
