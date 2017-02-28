cf524c7f8a75c5cc33e1b9b900584363c2a23a41
lucasbrambrink <lbrambrink@gmail.com>
2017-02-27 23:41:42 -0500

    automatic push

--- pwmanager/pwmanager/settings.py
+++ pwmanager/pwmanager/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     ('vault', os.path.join(BASE_DIR, 'vault', 'static')),
 )
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'vault', 'static')
 SHELL_PLUS_PRE_IMPORTS = (
     ('vault.utils', ('VaultConnection', 'PolicyApi', 'AppRoleApi', 'TokenApi', 'CreateUserPolicyApi')),
 )
